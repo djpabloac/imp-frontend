6bb4270e2a4a7565dff586f7767a1534da5ceba4
Pablo Arteaga <dj.pablo.ac@gmail.com>
2022-01-23 03:31:47 -0500

    [UPD] Config deploy 2

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+// Running PORT is set automatically by App Engine
+const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(__dirname+'/dist/imp-frontend'));
+const publicPath = path.join(__dirname, "/dist/imp-frontend");
 
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/imp-frontend/index.html'));
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/dist/imp-frontend/index.html"));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
