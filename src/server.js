1caacf505f2df0d4bf49a519ca8875f5d38e1aa3
Pablo Arteaga <dj.pablo.ac@gmail.com>
2022-01-23 03:21:00 -0500

    [UPD] Config deploy

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/imp-frontend'));
+
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/imp-frontend/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
